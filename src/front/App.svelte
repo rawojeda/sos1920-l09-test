97f73d590e134fcfb6219fb8403f8a79a1e4ed86
raw <raw@hotmail.com>
2020-05-04 10:29:05 +0200

    second working

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -1,35 +1,20 @@
 <script>
-	import MyContentTable from './MyContentTable.svelte';
-	export let name;
-	let myvar = 1;
 </script>
 
+	  
 <main>
-	<h1>Hello {name}!</h1>
-	<input bind:value={name}>
-	<MyContentTable />
-
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<h1>Team</h1>
+<h2>Raul Ojeda Robles</h2>
+<h2>Fran Zájara Gomez</h2>
+<p>	Project description: Our sources of information are aimed at analyzing the relationship between sugar consumption and the quantity of obese people</p>
+		
+<p>
+	Repository:  <a href="https://github.com/gti-sos/SOS1920-30">gti-sos/SOS1920-30</a>
+</p>
+	//<div class="div-home">
+		//<button type="button" class="btn btn-outline-success" onclick="window.location.href='#/evolution-of-cycling-routes'"> API Evolucion Carriles Bici</button>
+		//<button type="button" class="btn btn-outline-success" onclick="window.location.href='#/rural-tourism-stats'"> API Turismo Rural</button>
+		//<button type="button" class="btn btn-outline-success" onclick="window.location.href='#/traffic-accidents'"> API Accidentes De Trafico </button>
+	//</div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
--- src/front/index.js
+++ src/front/index.js
@@ -1,10 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.querySelector('#SvelteApp'),
-	props: {
-		name: 'raw'
-	}
+	target: document.querySelector('#SvelteApp')
 });
 
 export default app;
